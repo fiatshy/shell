a928e6e5974d81657c80b524c92ab021408ac317
fiatshy <toothyfgairy17@gmail.com>
2024-05-11 21:18:32 +0900

    cub update

--- cub/main.c
+++ cub/main.c
@@ -11,10 +11,10 @@
 #define	BG		0x00000000
 #define RED_DOT 0x00FF0000
 #define	BOX		0x00FFFFFF
-#define	WIDTH	100
-#define	HEIGHT	100
-#define MAP_WIDTH	2
-#define	MAP_HEIGHT	2
+#define	WIDTH	400
+#define	HEIGHT	400
+#define MAP_WIDTH	5
+#define	MAP_HEIGHT	5
 #define	SPEED	3
 #define	ROTATE_AMT	25
 
@@ -39,6 +39,12 @@ typedef	struct s_mlx
 	int			(*map)[MAP_WIDTH];
 }				t_mlx;
 
+double	abs_double(double x)
+{
+	if (x < 0)
+		return (-1 * x);
+	return (x);
+}
 
 void	put_pixel(t_img *img, int x, int y, int color)
 {
@@ -47,6 +53,30 @@ void	put_pixel(t_img *img, int x, int y, int color)
 	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
+
+void	draw_wall(t_mlx *tx, int x, int length)
+{
+	int	y = 0;
+	int	half_length = length / 2;
+	int	half_heigth = HEIGHT / 2;
+	int	count = 0;
+
+	y = half_heigth - half_length;
+
+	if (y < 0)
+		y = 0;
+
+	while (length)
+	{
+		if (count >= HEIGHT)
+			break;
+		put_pixel(tx->img, x, y, 0x00FFFFFF);
+		y++;
+		length--;
+		count++;
+	}
+}
+
 
 int	draw_line(t_mlx *tx, int begin_x, int begin_y, int end_x, int end_y, int color)
 {
@@ -81,10 +111,13 @@ void	*control_keys(int keycode, t_mlx *tx)
 	{
 		if (tx->pos[1] - SPEED >= 0)
 		{
-			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00000000);
-		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00000000);
-			tx->pos[0] += tx->dir[0];
-			tx->pos[1] -= tx->dir[1];
+			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00FF0000);
+			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FF0000);
+			tx->pos[0] += tx->dir[0] * 20;
+			tx->pos[1] -= tx->dir[1] * 20;
+			// tx->pos_arr[0] += tx->dir[0] * 0.3;
+			tx->pos_arr[1] -= tx->dir[1] * 0.25;
+			printf(" UP %f %f\n", tx->pos_arr[0], tx->pos_arr[1]);
 			put_pixel(tx->img, tx->pos[0], tx->pos[1], RED_DOT);
 			mlx_put_image_to_window(tx->mlx, tx->mlx_win, tx->img->img, 0, 0);
 		}
@@ -95,10 +128,13 @@ void	*control_keys(int keycode, t_mlx *tx)
 	{
 		if (tx->pos[1] + SPEED <= HEIGHT)
 		{
-			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00000000);
-			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00000000);
-			tx->pos[0] -= tx->dir[0];
-			tx->pos[1] += tx->dir[1];
+			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00FF0000);
+			draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FF0000);
+			tx->pos[0] += tx->dir[0] * 20;
+			tx->pos[1] += tx->dir[1] * 20;
+			// tx->pos_arr[0] += tx->dir[0] * 0.3;
+			tx->pos_arr[1] += tx->dir[1] * 0.25;
+			printf("DOWN %f %f\n", tx->pos_arr[0], tx->pos_arr[1]);
 			put_pixel(tx->img, tx->pos[0], tx->pos[1], RED_DOT);
 			mlx_put_image_to_window(tx->mlx, tx->mlx_win, tx->img->img, 0, 0);
 		}
@@ -107,8 +143,8 @@ void	*control_keys(int keycode, t_mlx *tx)
 	}
 	else if (keycode == LEFT)
 	{
-		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00000000);
-		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00000000);
+		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00FF0000);
+		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FF0000);
 		temp = tx->dir[0];
 		tx->dir[0] = tx->dir[0] * cos(-1 * ROTATE_AMT) - tx->dir[1] * sin(-1 * ROTATE_AMT);
 		tx->dir[1] = temp * sin(-1 * ROTATE_AMT) + tx->dir[1] * cos(-1 * ROTATE_AMT);
@@ -119,8 +155,8 @@ void	*control_keys(int keycode, t_mlx *tx)
 	}
 	else if (keycode == RIGHT)
 	{
-		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00000000);
-		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00000000);
+		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00FF0000);
+		draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FF0000);
 		temp = tx->dir[0];
 		tx->dir[0] = tx->dir[0] * cos(ROTATE_AMT) - tx->dir[1] * sin(ROTATE_AMT);
 		tx->dir[1] = temp * sin(ROTATE_AMT) + tx->dir[1] * cos(ROTATE_AMT);
@@ -133,7 +169,7 @@ void	*control_keys(int keycode, t_mlx *tx)
 	}
 }
 
-void	fill_minimap(void *data)
+void	fill_screen(void *data)
 {
 	int	i;
 	int	j;
@@ -151,46 +187,29 @@ void	fill_minimap(void *data)
 	}
 }
 
+void	fill_minimap(void *data)
+{
+	int	i;
+	int	j;
+	t_mlx	*tx;
+
+	i = 0;
+	j = 0;
+	tx = data;
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+			put_pixel(tx->img, i, j++, BG);
+		i++;
+	}
+}
+
 float	abs_float(float	x)
 {
 	if (x < 0)
 		return (x * -1);
 	return (x);
-}
-
-void	render_frame(void *data)
-{
-	t_mlx	*tx;
-
-	tx = data;
-	//float	x = 0.2;
-	//while (x < 0.66)
-	//{
-	//	float	tempX = tx->dir[0] + x;
-	//	float	tempY = tx->dir[1] + tx->plane[1];
-
-	//	float	unitX = sqrt(1 + ((tempY * tempY)/(tempX *tempX)));
-	//	float	unitY = sqrt(1 + ((tempX * tempX)/(tempY *tempY)));
-
-	//	float	distX = 0;
-	//	float	distY = 0;
-
-	//	distX += unitX;
-	//	distY += unitY;
-
-	//	float	scale = distX * 50 / distY;
-
-	//	float	lineX = tx->pos[0] + (tx->dir[0] * (int)scale) + (x * (int)scale);
-	//	float	lineY = tx->pos[1] - (tx->dir[1] * (int)scale) - (tx->plane[1] * (int)scale);
-		
-	//	draw_line(tx, tx->pos[0], tx->pos[1], lineX, lineY, 0x00FFFFFF);
-	//	draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FFFFFF);
-	//	x += 0.02;
-	//}
-	
-	draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00FFFFFF);
-	draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FFFFFF);
-	mlx_put_image_to_window(tx->mlx, tx->mlx_win, tx->img->img, 0, 0);
 }
 
 void	fill_obstacle(t_mlx *tx, int i, int j, int ppv)
@@ -220,7 +239,7 @@ void	fill_minimap_init(void *data)
 	int		pixel_per_value;
 
 	tx = data;
-	pixel_per_value = WIDTH / MAP_WIDTH; // 100 / 2 = 50
+	pixel_per_value = 100 / MAP_WIDTH; // 100 / 2 = 50
 	i = 0;
 	while (i < MAP_HEIGHT)
 	{
@@ -237,6 +256,102 @@ void	fill_minimap_init(void *data)
 	}
 }
 
+void	render_frame(void *data)
+{
+	t_mlx	*tx;
+
+	tx = data;
+	double x = 0;
+
+	double	rayX, rayY;
+	double	deltaX, deltaY;
+	double	distX, distY;
+	int		hit = 0;
+	int		mapX, mapY;
+	int		stepX, stepY;
+	int		side = 0;
+	double	converted_width = 0;
+
+	fill_screen(tx);
+	put_pixel(tx->img, tx->pos[0], tx->pos[1], RED_DOT);
+	while (x < WIDTH)
+	{
+		side = 0;
+		mapX = (int)tx->pos_arr[0];
+		mapY = (int)tx->pos_arr[1];
+
+		hit = 0;
+		converted_width = 2 * x / (double)WIDTH - 1;
+		rayX = tx->dir[0] + tx->plane[0] * converted_width;
+		rayY = tx->dir[1] + tx->plane[1] * converted_width;
+
+		deltaX = (rayX == 0) ? 1e30 : abs_double(1 / rayX);
+		deltaY = (rayY == 0) ? 1e30 : abs_double(1 / rayY);
+
+		if (rayX < 0)
+		{
+			stepX = -1;
+			distX = (tx->pos_arr[0] - mapX) * deltaX;
+		}
+		else
+		{
+			stepX = 1;
+			distX = (mapX + 1.0 - tx->pos_arr[0]) * deltaX;
+		}
+
+		if (rayY < 0)
+		{
+			stepY = 1;
+			distY = (mapY + 1.0 - tx->pos_arr[1]) * deltaY;
+		}
+		else
+		{
+			stepY = -1;
+			distY = (tx->pos_arr[1] - mapY) * deltaY;
+		}
+
+		while (hit == 0)
+		{
+			if (distX < distY)
+			{
+				distX += deltaX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				distY += deltaY;
+				mapY += stepY;
+				side = 1;
+			}
+			if (tx->map[mapY][mapX] == 1)
+			{
+				hit = 1;
+			}
+		}
+
+		double	final_length;
+		if (side == 0)
+		{
+			final_length = distX - deltaX;
+		}
+		else if (side == 1)
+		{
+			final_length = distY - deltaY;
+		}
+		float	draw_length = 1 / final_length * HEIGHT;
+		// printf("%d %d %d %f %f\n", mapY, mapX, side, final_length, draw_length);
+		draw_wall(tx, x, draw_length);
+		x += 1;
+	}
+	fill_minimap(tx);
+	fill_minimap_init(tx);
+	// put_pixel(tx->img, tx->pos[0], tx->pos[1], 0x00FF0000);
+	// draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10) + (tx->plane[0] * 10), tx->pos[1] - (tx->dir[1] * 10) - (tx->plane[1] * 10), 0x00FF0000);
+	// draw_line(tx, tx->pos[0], tx->pos[1], tx->pos[0] + (tx->dir[0] * 10), tx->pos[1] - (tx->dir[1] * 10), 0x00FF0000);
+	mlx_put_image_to_window(tx->mlx, tx->mlx_win, tx->img->img, 0, 0);
+}
+
 int	main(void)
 {
 	t_mlx	tx;
@@ -244,15 +359,18 @@ int	main(void)
 
 	int		map[MAP_HEIGHT][MAP_WIDTH] = 
 	{
-		{1, 1},
-		{0, 1}
+		{1, 1, 1, 1, 1},
+		{1, 1, 1, 1, 1},
+		{1, 1, 0, 1, 1},
+		{1, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1}
 	};
 
-	tx.pos[0] = 30;
-	tx.pos[1] = 90;
+	tx.pos[0] = 50;
+	tx.pos[1] = 80;
 
-	tx.pos_arr[0] = tx.pos[0] / 50;
-	tx.pos_arr[1] = tx.pos[1] / 50;
+	tx.pos_arr[0] = 2.5;
+	tx.pos_arr[1] = 4;
 
 	tx.dir[0] = 0;
 	tx.dir[1] = 1;
@@ -275,79 +393,9 @@ int	main(void)
 								&img.endian);
 
 	tx.img = &img;
-	put_pixel(tx.img, tx.pos[0], tx.pos[1], 0x00FF0000);
-	mlx_put_image_to_window(tx.mlx, tx.mlx_win, tx.img->img, 0, 0);
-	fill_minimap_init(&tx);
 
-	float x = 0.001;
 
-	float	rayX, rayY;
-	float	deltaX, deltaY;
-	float	distX, distY;
-	int		hit = 0;
-	int		mapX, mapY;
-	int		side = 0;
-
-	mapX = (int)tx.pos_arr[0];
-	mapY = (int)tx.pos_arr[1];
-
-	//while (x < 1)
-	//{
-		
-	//	x += 0.02;
-	//}
-
-	x = 0.4;
-	mapX = (int)tx.pos_arr[0];
-	mapY = (int)tx.pos_arr[1];
-	//printf("%d %d\n", mapX, mapY);
-	hit = 0;
-	rayX = tx.dir[0] + tx.plane[0] * x;
-	rayY = tx.dir[1] + tx.plane[1] * x;
-
-	deltaX = sqrt(1 + ((rayY * rayY)/(rayX * rayX)));
-	deltaY = sqrt(1 + ((rayX * rayX)/(rayY * rayY)));
-
-	distX = (mapX + 1.0 - tx.pos_arr[0]) * deltaX;
-	//distY = (mapY + 1.0 - tx.pos_arr[1]) * deltaY;
-	distY = (tx.pos_arr[1] - mapY) * deltaY;
-
-	printf("%f %f\n", deltaX, deltaY);
-	printf("%f %f\n", distX, distY);
-
-	while (hit == 0)
-	{
-		if (distX < distY)
-		{
-			distX += deltaX;
-			mapX += 1;
-			side = 0;
-		}
-		else
-		{
-			distY += deltaY;
-			mapY += -1;
-			side = 1;
-		}
-		if (tx.map[mapY][mapX] == 1)
-		{
-			hit = 1;
-		}
-	}
-	printf("%f %f %d %d %d =%d\n", distX, distY, mapX, mapY, tx.map[mapY][mapX], side);
-
-	//if (distX < distY)
-	//{
-	//	distX += deltaX;
-	//	mapY += 1;
-	//	side = 0;
-	//}
-	//else
-	//{
-	//	distY += deltaY;
-	//	mapX += -1;
-	//	side = 1;
-	//}
+	
 	
 	mlx_loop_hook(tx.mlx, render_frame, (void *)&tx);
 	mlx_key_hook(tx.mlx_win, control_keys, (void *)&tx);
