699b9461fa216992c3c09c24cc421b1602d08c06
fiatshy <sunghyki@c2r20s2.42gyeongsan.kr>
2024-05-27 14:39:48 +0900

    fix: echo related leak

--- srcs/fork/fork_parents.c
+++ srcs/fork/fork_parents.c
@@ -42,16 +42,31 @@ int	handle_res_nested(int *res, t_cmd_struct *tcst, int index)
 
 int	handle_res(int *res, t_cmd_struct *tcst, int index)
 {
+	int	length;
+
 	*res = is_builtin(tcst, index);
+	if (*res == 1)
+	{
+		ft_echo(tcst, index);
+		return (1);
+	}
 	if (*res == 2)
 	{
-		printf("exit\n");
-		free(tcst->tcmd[0]->arg[0]);
-		free(tcst->tcmd[0]->arg);
-		ft_lstclear(tcst->lst_env, free);
-		free(tcst->lst_env);
-		free_all(tcst);
-		exit(0);
+		length = 0;
+		while (tcst->tcmd[0]->arg[length])
+			length++;
+		if (length > 2)
+			printf("too many variables\n");	
+		else
+		{
+			printf("exit\n");
+			free(tcst->tcmd[0]->arg[0]);
+			free(tcst->tcmd[0]->arg);
+			ft_lstclear(tcst->lst_env, free);
+			free(tcst->lst_env);
+			free_all(tcst);
+			exit(0);
+		}
 	}
 	else if (*res == 3)
 	{
